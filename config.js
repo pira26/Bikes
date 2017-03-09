bca87b536ae0c873d2dfd6dec3fcd43fc517ed0a
Pirathaab <sritharan.pirathaab@gmail.com>
2017-03-09 17:59:18 +0100

    fix

--- config.js
+++ config.js
@@ -5,3 +5,6 @@ module.exports = {
   "dbUri": `mongodb://${process.env.USER_KEY}:${process.env.PASSWORD_KEY}@ds123080.mlab.com:23080/bikes`,
   "jwtSecret": "a secret phrase !"
 }
+
+console.log(process.env.USER_KEY);
+console.log(process.env.PASSWORD_KEY);
