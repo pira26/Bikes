38ca40146d11f9c4082b403c01cd0662badcc011
Pirathaab <sritharan.pirathaab@gmail.com>
2017-03-09 12:47:44 +0100

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const config = require('./config');
+const config = require('./config.js');
 
 // connect to the database and load models
 require('./server/models').connect(config.dbUri);
